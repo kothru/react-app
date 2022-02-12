36de172a0fe412bcd5a0d4cd2213703c5c317127
kothru <1894683+kothru@users.noreply.github.com>
2022-02-13 03:06:07 +0900

    add heading comp

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Heading } from "./libs/Heading"
 import { Text } from "./libs/Text"
 
 export const App = () => {
   return (
-    <Text text="true" />
+    <>
+      <Text text="true" />
+      <Heading tag="h1">
+        <span>hello</span>
+      </Heading>
+    </>
   )
 }
